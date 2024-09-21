8879f27103c45fa54cca1702c993e1acd1b48fbf
Shiva Shanmuganathan <shiva.shanmuganathan@srmuniv.edu.in>
2024-09-21 15:56:59 +0530

    Added AnecdoteList

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
-import AnecdoteForm from './components/AnecdoteForm'
-const App = () => {
-  // arrange anecdotes by number of votes
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
-  const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(voteAnecdote(id))
-  }
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+const App = () => {
+  
+  
+
+  
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
+
+      
       <AnecdoteForm />
+      
     </div>
   )
 }
