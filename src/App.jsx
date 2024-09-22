688c4af35f51a577021f64146f0ed5ec545469e0
Shiva Shanmuganathan <shiva.shanmuganathan@srmuniv.edu.in>
2024-09-22 16:15:08 +0530

    Added local DB and added read and write logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import AnecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    AnecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,16 +1,18 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { showNotification } from '../reducers/notificationReducer'
+import AnecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
-        event.preventDefault()
-        const content = event.target.anecdote.value
-        event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
-        dispatch(showNotification(`New anecdote '${content}' created`, 5))
+    const addAnecdote = async (event) => {
+      event.preventDefault()
+      const content = event.target.anecdote.value
+      event.target.anecdote.value = ''
+      const newAnecdote = await AnecdoteService.createNew(content)
+      dispatch(createAnecdote(newAnecdote))
+      dispatch(showNotification(`New anecdote '${content}' created`, 5))
     }
 
     return (
